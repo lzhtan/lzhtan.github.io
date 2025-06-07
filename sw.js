1006f9217f9ee2ba2dfbbaa07897497abaca8dd8
Tan <38093710+lzhtan@users.noreply.github.com>
2025-06-07 23:52:43 +0900

    up

---
+++ sw.js
@@ -0,0 +1,69 @@
+// Service Worker for caching optimization
+const CACHE_NAME = 'lzhtan-site-v1';
+const urlsToCache = [
+  '/',
+  '/index_cn.html',
+  '/css/jemdoc.css',
+  '/img/tan.jpg',
+  '/img/netmeasurement.jpeg',
+  '/img/nettesting.jpeg',
+  '/img/netmanagement.jpeg',
+  '/img/logo.ico'
+];
+
+// Install event - cache resources
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event - serve from cache when possible
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Return cached version or fetch from network
+        if (response) {
+          return response;
+        }
+        
+        return fetch(event.request).then(function(response) {
+          // Check if we received a valid response
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          // Clone the response
+          var responseToCache = response.clone();
+
+          caches.open(CACHE_NAME)
+            .then(function(cache) {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        });
+      })
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+}); 
